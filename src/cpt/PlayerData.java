95170badbe9fe739c95e36f4b59760eabb8f4a1f
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2023-01-23 10:39:42 -0500

    new file

---
+++ src/cpt/PlayerData.java
@@ -0,0 +1,15 @@
+package cpt;
+
+public class PlayerData {
+
+    private String strPlayerName; 
+    private double dblPercentage;
+    private String strPosition;
+
+    public PlayerData(String strPlayerName, double dblPercentage, String strPosition){
+        this.strPlayerName = strPlayerName;
+        this.dblPercentage = dblPercentage;
+        this.strPosition = strPosition;
+    }
+
+}
