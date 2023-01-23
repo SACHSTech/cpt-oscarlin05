d9480516c7bd2d28d70a1be6cec3cd839c8c62d2
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2023-01-23 10:39:55 -0500

    created team data

---
+++ src/cpt/TeamData.java
@@ -0,0 +1,16 @@
+package cpt;
+
+import java.util.ArrayList;
+
+public class TeamData {
+
+    private String strTeams;
+    private ArrayList<Integer> intPercentage = new ArrayList<>();
+
+    public TeamData(String strTeams, int intPercentage) {
+        this.strTeams = strTeams;
+        
+    }
+
+    
+}
