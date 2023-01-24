81b4945b32e2f82a51cef3a21d3f62ca3db5b56f
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2023-01-24 09:56:05 -0500

    added getter method

--- src/cpt/PlayerData.java
+++ src/cpt/PlayerData.java
@@ -4,15 +4,21 @@ public class PlayerData {
 
     private String strPlayerName; 
     private double dblPercentage;
+    private String strTeamName;
     private String strPosition;
 
-    public PlayerData(String strPlayerName, double dblPercentage, String strPosition){
+    public PlayerData(String strPlayerName, String strTeamName, double dblPercentage, String strPosition){
         this.strPlayerName = strPlayerName;
+        this.strTeamName = strTeamName;
         this.dblPercentage = dblPercentage;
         this.strPosition = strPosition;
     }
     public String getPlayerName() {
         return strPlayerName;
+    }
+
+    public String getTeamName() {
+        return strTeamName;
     }
 
     public double getPercentage() {
@@ -22,5 +28,6 @@ public class PlayerData {
     public String getPosition() {
         return strPosition;
     }
+
 
 }
