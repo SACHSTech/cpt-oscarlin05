e60ff706aa590314498989d2d9186973b8783df7
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2023-01-25 01:08:06 -0500

    comments and javadocs

--- src/cpt/PlayerData.java
+++ src/cpt/PlayerData.java
@@ -1,13 +1,24 @@
 package cpt;
 
+/**
+ * A class made for player data with their percentage, team name, and rankings 
+ */
 public class PlayerData {
 
+    // create instance variables 
     private int intRanking;
     private String strPlayerName; 
     private double dblPercentage;
     private String strTeamName;
     private String strPosition;
 
+    /**
+     * 
+     * @param intRanking the ranking of the player
+     * @param strPlayerName the name of the player 
+     * @param strTeamName the team name of the place 
+     * @param dblPercentage the free throw percentage 
+     */
     public PlayerData(int intRanking, String strPlayerName, String strTeamName, double dblPercentage){
         this.intRanking = intRanking;
         this.strPlayerName = strPlayerName;
@@ -15,18 +26,34 @@ public class PlayerData {
         this.dblPercentage = dblPercentage;
     }
 
+    /**
+     * accessor method for ranking 
+     * @return intRanking 
+     */
     public int getRanking(){
         return intRanking;
     }
 
+    /**
+     * accessor method for player name
+     * @return strPlayerName
+     */
     public String getPlayerName() {
         return strPlayerName;
     }
 
+    /**
+     * accessor method for team name 
+     * @return strTeamName
+     */
     public String getTeamName() {
         return strTeamName;
     }
 
+    /**
+     * accessor method for freethrow percentage
+     * @return dblPercentage
+     */
     public double getPercentage() {
         return dblPercentage;
     }
